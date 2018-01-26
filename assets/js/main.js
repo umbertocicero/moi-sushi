8554a99b3b2aa09c6639bf90228fb80bc697e8f9
umberto <umbertocicero@gmail.com>
2018-01-26 15:34:51 +0100

    FLOATING "BACK TO TOP" BUTTON

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,6 +77,22 @@ jQuery(document).ready(function ($) {
                 );
     });
 
+	/* ---------------------------------------------------------------------
+     FLOATING "BACK TO TOP" BUTTON
+     ---------------------------------------------------------------------= */
+	$(document).ready(function(){ 
+		$(window).scroll(function(){ 
+			if ($(this).scrollTop() > 100) { 
+				$('.scroll-top').fadeIn(); 
+			} else { 
+				$('.scroll-top').fadeOut(); 
+			} 
+		}); 
+		$('.scroll-top').click(function(){ 
+			$("html, body").animate({ scrollTop: 0 }, 600); 
+			return false; 
+		}); 
+	});
     /* ---------------------------------------------------------------------
      Carousel
      ---------------------------------------------------------------------= */
