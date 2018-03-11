35d43c90866010c480a890bb80d1d48064f183a9
umbertocicero@gmail.com <umbertocicero@gmail.com>
2018-03-11 13:44:21 +0100

    google-search.js

---
+++ assets/js/google-search.js
@@ -0,0 +1,41 @@
+{
+  "@context": "http://schema.org",
+  "@type": "Restaurant",
+  "image": [
+    "https://moiprato.it/assets/images/logo_oriz_trasp_black.png"
+   ],
+  "@id": "https://moiprato.it",
+  "name": "Moi",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Viale piave, 10",
+    "addressLocality": "Prato",
+    "addressRegion": "PO",
+    "postalCode": "59100",
+    "addressCountry": "IT"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 43.8783484,
+    "longitude": 11.0963779
+  },
+  "url": "https://moiprato.it",
+  "telephone": "+390574065595",
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ],
+      "opens": "18:30",
+      "closes": "23:00"
+    }
+  ],
+  "acceptsReservations": "True"
+}
